70d496a9284301804e7a592dc6257490eaaa8157
Guillaume Castonguay <guillaume.castonguay@gmail.com>
2013-07-30 16:34:23 -0400

    Fin de journée

--- app/controllers/room_equipments_controller.rb
+++ app/controllers/room_equipments_controller.rb
@@ -1,9 +1,21 @@
 class RoomEquipmentsController < ApplicationController
+ 
+ def index
+  @room = Room.find(params[:room_id])
+  #@room_equip = @room.room_equipments.all
+ end 
+
  def create 
   @room = Room.find(params[:room_id])
 
   @room_equipment = @room.room_equipments.create(params[:room_equipment].permit(:tag, :quantity))
   redirect_to room_path(@room)
  end
+
+ def edit
+ @room = Room.find(params[:room_id]) 
+ @room_equipment = @room.room_equipments.find(params[:id]) 
+ end
+
 
 end
